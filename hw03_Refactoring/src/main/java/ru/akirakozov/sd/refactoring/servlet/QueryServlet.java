158e9460d36b80c6ad1016b2054dbfc8685b433c
Andrey <andreystar2403@gmail.com>
2021-10-22 16:46:47 +0300

    Aggregate html result in a string, then write to ResponseWriter.

--- hw03_Refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ hw03_Refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Optional;
 
 /**
@@ -24,61 +25,58 @@ public class QueryServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String command = request.getParameter("command");
 
+        StringBuilder html = new StringBuilder();
         if ("max".equals(command)) {
             try {
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("<h1>Product with max price: </h1>");
+                html.append("<html><body>\n");
+                html.append("<h1>Product with max price: </h1>\n");
                 Optional<Product> res = productDAO.getMaxProduct();
-                if (res.isPresent()) {
-                    response.getWriter().println(res.get().getName() + "\t" + res.get().getPrice() + "</br>");
-                }
-                response.getWriter().println("</body></html>");
-            } catch (Exception e) {
+                res.ifPresent(product -> html.append(product.getName())
+                                             .append("\t")
+                                             .append(product.getPrice())
+                                             .append("</br>\n"));
+                html.append("</body></html>\n");
+            } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         } else if ("min".equals(command)) {
             try {
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("<h1>Product with min price: </h1>");
+                html.append("<html><body>\n");
+                html.append("<h1>Product with min price: </h1>\n");
                 Optional<Product> res = productDAO.getMinProduct();
-                if (res.isPresent()) {
-                    response.getWriter().println(res.get().getName() + "\t" + res.get().getPrice() + "</br>");
-                }
-                response.getWriter().println("</body></html>");
-            } catch (Exception e) {
+                res.ifPresent(product -> html.append(product.getName())
+                                             .append("\t")
+                                             .append(product.getPrice())
+                                             .append("</br>\n"));
+                html.append("</body></html>\n");
+            } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         } else if ("sum".equals(command)) {
             try {
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("Summary price: ");
+                html.append("<html><body>\n");
+                html.append("Summary price: \n");
                 Optional<Integer> sum = productDAO.getProductPriceSum();
-                if (sum.isPresent()) {
-                    response.getWriter().println(sum.get());
-                }
-                response.getWriter().println("</body></html>");
-            } catch (Exception e) {
+                sum.ifPresent(integer -> html.append(integer).append("\n"));
+                html.append("</body></html>\n");
+            } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         } else if ("count".equals(command)) {
             try {
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("Number of products: ");
+                html.append("<html><body>\n");
+                html.append("Number of products: \n");
                 Optional<Integer> count = productDAO.getProductCount();
-                if (count.isPresent()) {
-                    response.getWriter().println(count.get());
-                }
-                response.getWriter().println("</body></html>");
-            } catch (Exception e) {
+                count.ifPresent(integer -> html.append(integer).append("\n"));
+                html.append("</body></html>\n");
+            } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         } else {
-            response.getWriter().println("Unknown command: " + command);
+            html.append("Unknown command: ").append(command).append("\n");
         }
-
+        response.getWriter().println(html);
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
     }
-
-
 }
